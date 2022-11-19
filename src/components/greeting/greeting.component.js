b828be9dc1cd163e79b10a41def2118339c47bbb
Gabriel Santo <gvgesanto2@gmail.com>
2022-11-19 19:01:42 -0300

    feat: Create the React initial app

---
+++ src/components/greeting/greeting.component.js
@@ -0,0 +1,18 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchGreetings } from '../../redux/greeting/greeting.reducer';
+
+export default function Greeting() {
+  const dispatch = useDispatch();
+  const { greeting } = useSelector((state) => state.greeting);
+
+  useEffect(() => {
+    dispatch(fetchGreetings());
+  }, []);
+  return (
+    <>
+      <h1>Greeting</h1>
+      <p>{greeting}</p>
+    </>
+  )
+}
